a4b5599a538b9e55c6fb35c1cee1d52c344670d8
YasinU1 <sayedmuhammadyasin.usman@vodafone.com>
2023-02-09 00:57:31 +0000

    feature: Added Login functionality, by implementing the connection between our Mongo database and BE API endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect  } from "react";
 import "./App.css";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
@@ -13,19 +13,28 @@ import Subscribe from "./pages/Subscribe/Subscribe";
 export interface AuthenticationProps {
   isAuthenticated: boolean;
 }
-//The App uses React Router to handle the page routing
+
 const App = ({ isAuthenticated }: AuthenticationProps) => {
+  useEffect(() => {
+    // What this does is adds the authenticated parameter to local storage so that a user no longer needs to
+    // login each time a different section of the site is clicked unless new window is open 
+    // This is done by saving the authentication status in local storage as a string
+    localStorage.setItem("isAuthenticated", JSON.stringify(isAuthenticated));
+    // Remove the authentication status from local storage when the user closes the window
+    window.addEventListener("beforeunload", () => {
+      localStorage.removeItem("isAuthenticated");
+    });
+  }, [isAuthenticated]);
+
   return (
     <BrowserRouter>
-      {/*If the user is not authenticated then the app will display the login page*/}
-      {/*{!isAuthenticated && (*/}
-      {/*  <Routes>*/}
-      {/*    <Route path="/" element={<Login />} />*/}
-      {/*    <Route path="*" element={<Navigate to="/" replace />} />*/}
-      {/*  </Routes>*/}
-      {/*)}*/}
-      {/*If the user is authenticated then the app will allow the user to access the rest of the pages*/}
       {!isAuthenticated && (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      )}
+      {isAuthenticated && (
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -34,13 +43,23 @@ const App = ({ isAuthenticated }: AuthenticationProps) => {
             <Route path="/films" element={<Films />} />
             <Route path="/subscribe" element={<Subscribe />} />
             <Route path="*" element={<Navigate to="/" />} />
-          </Routes>{" "}
+          </Routes>
         </Layout>
       )}
     </BrowserRouter>
   );
 };
-const mapStateToProps = (state: RootState) => ({
-  isAuthenticated: state.auth.queries.user?.status === "fulfilled",
-});
+
+// This just checks if the user is authenticated from local storage or from the state
+// and then returns the authentication as a prop to be used during routing
+const mapStateToProps = (state: RootState) => {
+  const isAuthenticated =
+    JSON.parse(localStorage.getItem("isAuthenticated") || "false") ||
+    state.auth.queries.user?.status === "fulfilled";
+
+  return {
+    isAuthenticated,
+  };
+};
+
 export default connect(mapStateToProps)(App);
--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,25 @@
-
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import path from "path";
 import cors from "cors";
 import fetch from 'isomorphic-fetch';
-//tests
+import axios from 'axios';
+
+//This configures environment variables using dotenv
 dotenv.config();
 
+// Just some middleware to create the Express app and run modules
 const app: Express = express();
-
 app.use(express.json());
 app.use(cors());
 
-
+// Headers for HTTP requests for the mongodb 
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Access-Control-Request-Headers", "*");
 myHeaders.append("api-key", "2pLVyVoLai1QVk1AZwfAC1UvcueRt3mX9ha7J3SbQxIvbwmWij3WJgGqMlkd3Ix2");
 myHeaders.append("Accept", "application/json");
-
+// HTTP request options for fetching data from a MongoDB database for the collection films to display all.
 const raw = JSON.stringify({
   "dataSource": "Cluster0",
   "database": "Films",
@@ -33,6 +34,22 @@ const requestOptions: RequestInit = {
   redirect: 'follow'
 };
 
+// HTTP request options for fetching data from a MongoDB database for the collection : customers to display all.
+const rawCustomers = JSON.stringify({
+  "dataSource": "Cluster0",
+  "database": "Films",
+  "collection": "customers",
+  "filter": {}
+});
+
+const requestOptionsCustomers: RequestInit = {
+  method: 'POST',
+  headers: myHeaders,
+  body: rawCustomers,
+  redirect: 'follow'
+};
+
+// API endpoint to retrieve data from a MongoDB database that shows all the films
 app.get('/api', (req, res) => {
     fetch("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptions)
         .then(res => res.json())
@@ -44,52 +61,67 @@ app.get('/api', (req, res) => {
             res.status(500).send('Error: ' + error);
         });
 });
-
-app.get('/', (req: Request, res: Response) => {
-res.send('<h1>Hello World From the Typescript Server!</h1>')
+// API endpoint to retrieve customer data from a MongoDB database
+app.get('/users', (req, res) => {
+  fetch("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptionsCustomers)
+      .then(res => res.json())
+      .then(data => {
+          res.json(data)
+      })
+      .catch(error => {
+          console.log('error', error);
+          res.status(500).send('Error: ' + error);
+      });
 });
 
-const port = process.env.PORT || 8000;
+// Define a GET endpoint at the default URL of the app
+app.get('/', (req: Request, res: Response) => {
+  // with a dummy html message
+  res.send('<h1>Hello World From the Typescript Server!</h1>')
+  });
 
+// Set the server's port number
+// Use the value of an environment variable named 'PORT', or fallback to 8000 if not set  
+const port = process.env.PORT || 8000;
+// This starts the app and listen on the specified port 
+// and logs a message to the console indicating the app is listening on the specified port
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 });
-
-
+// Creates an empty array to store fetched customer data
+let customerArray: any[] = [];
+// Asynchronously fetch customer data from the specified URL
+async function fetchData() {
+  // Asynchronously fetch customer data from the specified URL, 
+  // and it then maps the fetched data to a new format and store it in the `customerArray` array
+  const res = await axios.get('http://localhost:8000/users');
+  customerArray = res.data.documents.map((customer: {_id: string, fname: string, sname: string, email: string,password: string}) => {
+    return {
+      _id: customer._id,
+      name: customer.fname + " " + customer.sname,
+      email: customer.email,
+      password: customer.password
+    }
+  });
+}
+// A simple call method to make the function above run
+fetchData();
 interface FormInputs {
     email: string,
     password: string
   }
 
-  // Array of example users for testing purposes
-  const users = [
-    {
-      id: 1,
-      name: 'Maria Doe',
-      email: 'maria@example.com',
-      password: 'maria123'
-    },
-    {
-      id: 2,
-      name: 'Juan Doe',
-      email: 'juan@example.com',
-      password: 'juan123'
-    }
-  ];
-
-  // route login
-  app.post('/login', (req: Request, res: Response) => {
-    const { email, password }:FormInputs = req.body;
-
-    const user = users.find(user => {
-      return user.email === email && user.password === password
-    });
-
-    if (!user) {
-      return res.status(404).send('User Not Found!')
-    }
-
-    return res.status(200).json(user)
+// Define a POST endpoint at the '/login' URL of the app so it can authenticate the login system of the site and works with the front end
+app.post('/login', (req: Request, res: Response) => {
+  const { email, password }:FormInputs = req.body;
+  // Search the `customerArray` which is the same as mongodb for a user with matching email and password
+  const user = customerArray.find(user => {
+    return user.email === email && user.password === password
   });
+  // If no matching user is found, return a 404 status and a 'User Not Found' message
+  if (!user) {
+    return res.status(404).send('User Not Found!')
+  }
 
-  
+  return res.status(200).json(user)
+});
