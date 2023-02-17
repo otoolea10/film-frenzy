3b1ddba640774acb66eb42668b40ad23a10334c5
otoola10 <a.otoole@aol.co.uk>
2023-02-17 16:54:49 +0000

    add styling to user table

---
+++ client/src/components/UseTable/UserTableStyles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const UserTabelStyles = styled.div`
+  width: 70%;
+  margin: auto;
+  padding: 1rem 0;
+  table,
+  th,
+  td {
+    border: 1px solid #27323f;
+    border-collapse: collapse;
+  }
+  td,
+  th {
+    padding: 1rem;
+    color: #27323f;
+  }
+`;
