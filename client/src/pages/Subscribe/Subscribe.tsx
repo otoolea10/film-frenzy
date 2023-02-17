2ba72a7d205e78138d8030c5b2896afa705e275f
otoola10 <a.otoole@aol.co.uk>
2023-02-17 13:11:09 +0000

    adds a subscription page

--- client/src/pages/Subscribe/Subscribe.tsx
+++ client/src/pages/Subscribe/Subscribe.tsx
@@ -5,14 +5,15 @@ import lensBanner from "../../../public/assets/images/banners/lens-banner.jpg";
 import HeroBanner from "../../components/HeroBanner/HeroBanner";
 
 //Subscription component used to display the subscription plans
-const Home = () => {
+const Subscription = () => {
   return (
     <>
       <HeroBanner heroBannerImage={lensBanner} />
       {/*Skinny banner to introduce the subscription plans*/}
       <SkinnyBanner bannerHeading="Choose a Subscription Plan" />
+      {/*Subcards to display the available subscription plans and their information*/}
       <SubCards />
     </>
   );
 };
-export default Home;
+export default Subscription;
