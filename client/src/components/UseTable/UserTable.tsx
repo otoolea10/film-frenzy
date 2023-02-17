76dae123c924257a29ebaf6e34f32b6b95d2a512
otoola10 <a.otoole@aol.co.uk>
2023-02-17 16:53:38 +0000

    add users table component

--- client/src/components/UseTable/UserTable.tsx
+++ client/src/components/UseTable/UserTable.tsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { Users } from "../../store/usersApi";
+import { UserTabelStyles } from "./UserTableStyles";
 
 const UserTable = ({ userData }: { userData: Array<Users> }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Email</th>
-          <th>Username</th>
-          <th>FirstName</th>
-          <th>Surname</th>
-          <th>Date of Birth</th>
-        </tr>
-      </thead>
-      <tbody>
-        {userData &&
-          userData.map(({ _id, email, username, fname, sname, DOB }) => (
-            <tr>
-              <td>{_id}</td>
-              <td>{email}</td>
-              <td>{username}</td>
-              <td>{fname}</td>
-              <td>{sname}</td>
-              <td>{new Date(DOB).toDateString()}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <UserTabelStyles>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>FirstName</th>
+            <th>Surname</th>
+            <th>Date of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData &&
+            userData.map(({ _id, email, username, fname, sname, DOB }) => (
+              <tr>
+                <td>{_id}</td>
+                <td>{email}</td>
+                <td>{username}</td>
+                <td>{fname}</td>
+                <td>{sname}</td>
+                <td>{new Date(DOB).toDateString()}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </UserTabelStyles>
   );
 };
 export default UserTable;
