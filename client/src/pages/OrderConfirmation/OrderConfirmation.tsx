fb946c02b465d40451f6aa8f78dedfb70089f9c3
otoola10 <a.otoole@aol.co.uk>
2023-02-17 13:09:05 +0000

    adds a order confirmation page

--- client/src/pages/OrderConfirmation/OrderConfirmation.tsx
+++ client/src/pages/OrderConfirmation/OrderConfirmation.tsx
@@ -5,6 +5,8 @@ import SkinnyBanner from "../../components/SkinnyBanner/SkinnyBanner";
 import { useParams } from "react-router-dom";
 import { useGetSubscriptionByIdQuery } from "../../store/subscriptionApi";
 
+//Once a user has selected a plan and entered their details they will be shown the order confirmation page
+//This page shows the user which plan they have ordered by pulling the subscription id from the url
 const OrderConfirmation = () => {
   const { planId } = useParams();
   const { isLoading, data } = useGetSubscriptionByIdQuery(planId!);
